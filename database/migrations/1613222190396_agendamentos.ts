8e5c02ba163981a0b9398ee44b347c7ec0ac4255
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2021-02-14 13:02:18 -0300

    relacionamentos de tabela finalizados

--- database/migrations/1613222190396_agendamentos.ts
+++ database/migrations/1613222190396_agendamentos.ts
@@ -8,11 +8,19 @@ export default class Agendamentos extends BaseSchema {
       table.increments('id')
       table.enum('status', ['finalizado', 'cancelado', 'pendente'])
       table
+      .integer('treinamento_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('treinamentos')
+      table
       .integer('cliente_id')
       .unsigned()
       .notNullable()
       .references('id')
       .inTable('clientes')
+      table.string('observacao')
+      table.string('agendado_por').nullable()
       table.dateTime('agendado_em')
     })
   }
