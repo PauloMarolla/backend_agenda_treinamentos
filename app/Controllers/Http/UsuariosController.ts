87ee3729c14c6d420db6a105363d258585eb75be
Paulo Marolla <paulo.ricardo.marolla@hotmail.com>
2021-02-17 19:39:47 -0300

    validacao usuario

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,5 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import Usuario from 'App/Models/Usuario'
+
 export default class UsuariosController {
   async index() {
     let users = await Usuario.all()
@@ -8,7 +10,28 @@ export default class UsuariosController {
   }
 
   async store({request}: HttpContextContract) {
-    let user = await Usuario.create(request.all())
+
+    const usuarioSchema = schema.create({
+      nome: schema.string({}, [
+        rules.minLength(3)
+      ]),
+      email: schema.string({}, [
+        rules.email(),
+        rules.unique({table: 'usuarios', column: 'email'})
+      ]),
+      empresa: schema.enum(['SS', 'EC']),
+      grupo: schema.enum(['admin', 'lider', 'usuario', 'convidado']),
+      password: schema.string({}, [
+        rules.required()
+      ])
+    })
+
+    const validatedData = await request.validate({schema: usuarioSchema, messages: {
+      'nome': 'nao sei',
+      'email.unique' : 'Seu email já está em uso'
+    }})
+
+    let user = await Usuario.create(validatedData)
 
     return user;
 
